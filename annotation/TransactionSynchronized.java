feffb756aeb1a8719964fb152e9d00b3e35116e2
romainlavabre <romainlavabre98@gmail.com>
2021-12-22 15:25:53 +0100

    INITIAL

---
+++ annotation/TransactionSynchronized.java
@@ -0,0 +1,12 @@
+package com.replace.replace.api.upload.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention( RetentionPolicy.RUNTIME )
+@Target( ElementType.FIELD )
+public @interface TransactionSynchronized {
+    boolean value() default true;
+}
